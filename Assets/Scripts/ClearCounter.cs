a6e79f11c6c520c2be0fc179610d9abc09ccd0d3
Steve Mackidd <55397135+smackidd@users.noreply.github.com>
2024-09-09 22:24:53 -0600

    Allow player to drop kitchen object on clearCounter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -12,7 +12,32 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        
+        if (!HasKitchenObject())
+        {
+            //there is no kitchen object here
+            if (player.HasKitchenObject())
+            {
+                //Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                //Player not carrying anything
+            }
+        }  
+        else
+        {
+            //There is a kitchen object here
+            if (player.HasKitchenObject())
+            {
+                //Player is carrying something
+            }
+            else
+            {
+                //Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
     
